a864e08a2bfe7a07909048a4686bacf24763ea87
hellopuza <pluton11a@gmail.com>
2023-11-25 13:02:39 +0300

    Structurized commands and \help added

--- dabudimesh/main.py
+++ dabudimesh/main.py
@@ -1,63 +1,7 @@
 from router import Router
-from utils import (
-    create_bluetooth_server,
-    create_tcp_server,
-    create_bluetooth_connection,
-    create_tcp_connection,
-    socket_address,
-)
+from utils import create_server, socket_address
+from shell import DabudiShell, _on_read
 import asyncio
-import sys
-import bluetooth
-from config import IS_USING_BLUETOOTH
-
-EXIT_COMMAND = "\\exit"
-CONNECT_COMMAND = "\\connect"
-MESSAGE_COMMAND = "\\message"
-SCAN_COMMAND = "\\scan"
-
-
-def _on_command(sock, router):
-    command = sock.readline()
-    command = command.strip()
-    print(f"Processing command: {command}")
-    if command.startswith(EXIT_COMMAND):
-        print("Got exit command")
-        exit()
-
-    elif command.startswith(CONNECT_COMMAND):
-        print("Got connect command")
-        addr = command.split(" ")[1]
-        sock = (
-            create_bluetooth_connection(addr)
-            if IS_USING_BLUETOOTH
-            else create_tcp_connection(addr)
-        )
-        router.add_connection(addr, sock)
-        asyncio.get_event_loop().add_reader(sock, _on_read, sock, router)
-
-    elif command.startswith(MESSAGE_COMMAND):
-        print("Got message command")
-        [_, address, text] = command.split(" ", maxsplit=2)
-        router.send(address, text)
-
-    elif command.startswith(SCAN_COMMAND):
-        print("Got scan command")
-        devices = dict(bluetooth.discover_devices(lookup_names=True))
-        devices = {devices[k]: k for k in devices}
-
-        print(f"Found {len(devices)} devices")
-        for name in devices:
-            print(devices[name], name)
-
-    else:
-        print(f"Can't handle command {command}")
-
-
-def _on_read(sock, router):
-    text = router.process_message_from(sock)
-    if text is not None:
-        print(text)
 
 
 def _on_accept(router):
@@ -66,14 +10,14 @@ def _on_accept(router):
 
 
 def main():
-    listener = create_bluetooth_server() if IS_USING_BLUETOOTH else create_tcp_server()
-    address = socket_address(listener, IS_USING_BLUETOOTH)
+    listener = create_server()
+    address = socket_address(listener)
     router = Router(address, listener)
-    print("Our address is ", address)
 
     loop = asyncio.new_event_loop()
+    shell = DabudiShell(router, loop)
+    loop.run_in_executor(None, shell.cmdloop)
     loop.add_reader(listener, _on_accept, router)
-    loop.add_reader(sys.stdin, _on_command, sys.stdin, router)
     loop.run_forever()
 
 
---
+++ dabudimesh/shell.py
@@ -0,0 +1,70 @@
+from utils import create_connection
+import bluetooth
+from cmd import Cmd
+
+
+def _on_read(sock, router):
+    text = router.process_message_from(sock)
+    if text is not None:
+        print(text)
+
+
+class DabudiShell(Cmd):
+    type_help = "Type help or ? to list commands."
+    intro = "Welcome to the DabudiMesh shell. " + type_help + "\n"
+    prompt = "(dabudi) "
+
+    def __init__(self, router, event_loop):
+        super().__init__()
+        self.router = router
+        self.event_loop = event_loop
+
+    def do_exit(self, arg):
+        "exit : Terminate this program"
+        self.__process(arg, 0)
+        print("Thank you for using DabudiMesh")
+        if self.event_loop is not None:
+            self.event_loop.stop()
+        return True
+
+    # TODO: add disconnect command
+    def do_connect(self, arg):
+        "connect [addr] : Connect to node with specified address (addr)"
+        args = self.__process(arg, 1)
+        if args is not None:
+            addr = args[0]
+            sock = create_connection(addr)
+            self.router.add_connection(addr, sock)
+            if self.event_loop is not None:
+                self.event_loop.add_reader(sock, _on_read, sock, self.router)
+
+    def do_message(self, arg):
+        "message [addr] [msg] : Send message (msg) to node with address (addr)"
+        args = self.__process(arg, 2)
+        if args is not None:
+            self.router.send(args[0], args[1])
+
+    def do_scan(self, arg):
+        "scan : Discover nearby bluetooth devices"
+        args = self.__process(arg, 0)
+        if args is not None:
+            devices = dict(bluetooth.discover_devices(lookup_names=True))
+            devices = {devices[k]: k for k in devices}
+            print(f"Found {len(devices)} devices")
+            for name in devices:
+                print(devices[name], name)
+
+    def __process(self, arg, num):
+        args = arg.split(" ", maxsplit=num - 1) if len(arg) else arg
+        args_len = len(args)
+        if args_len == num:
+            return args
+
+        print(
+            f"This command have a number of {num} arguments but {args_len} were given"
+        )
+        print(self.type_help)
+        return None
+
+    def preloop(self):
+        print(f"Our address is {self.router.address}")
