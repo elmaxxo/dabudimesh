e38280c8e0a808a7a26bb6e7176c367eab5f97bd
khamitov02 <khamitov02@inbox.ru>
2023-12-01 23:31:34 +0300

    Security added

--- dabudimesh/network.py
+++ dabudimesh/network.py
@@ -5,12 +5,16 @@ from utils import run_event_loop
 from config import MSG_MAX_LEN
 import asyncio
 from threading import Thread
-
+from cyphers.kyber  import  Kyber768
+from cyphers.aescypher import AESCipher
 
 # TODO: replace output list with logger
 class NetworkInterfaceController:
     def __init__(self, output: list):
         self.output = output
+        self.public_key = None
+        self.shared_keys={}
+        self.private_keys={}
         listener = create_server()
         address = socket_address(listener)
         self.router = Router(address)
@@ -31,6 +35,10 @@ class NetworkInterfaceController:
         greeting = Message("greeting", self.router.address, addr)
         sock.sendall(greeting.encode())
 
+        public_key, private_key = Kyber768.keygen()
+        self.private_keys[addr] = private_key
+        self.public_key = public_key
+        
         message = Message.decode(sock.recv(MSG_MAX_LEN))
         assert message.get_command() == "routes"
         addr_to = message.get_source()
@@ -45,8 +53,18 @@ class NetworkInterfaceController:
             self.event_loop.add_reader, sock, self.__on_read, addr
         )
         return True
+    def send_public_key(self,dst_addr,pk):
+        message = Message("public_key", self.router.address, dst_addr, {"public_key": list(pk)})
+        self.router.send(message)
+        print("Public key is sended")
 
+    def send_cypher_text(self,dst_addr,st):
+        message = Message("cypher_text", self.router.address, dst_addr, {"cypher_text": list(st)})
+        self.router.send(message)
+        print("Cypher text is sended")
+        
     def send_text(self, dst_addr, text):
+        
         message = Message("text", self.router.address, dst_addr, {"text": text})
         self.router.send(message)
 
@@ -71,11 +89,15 @@ class NetworkInterfaceController:
             message = self.router.route_pending()
             if message is None:
                 return
-
             if message.get_command() == "text":
                 text = message.get_param("text")
-                self.output.append(f"Message from {message.get_source()}: {text}")
-
+                if message.get_source() in self.shared_keys:
+                    cypher = AESCipher(self.shared_keys[message.get_source()])
+                    print(self.shared_keys[message.get_source()])
+                    message_decoded=cypher.decrypt(text).decode("utf-8")
+                    self.output.append(f"Decrypted message from {message.get_source()}: {message_decoded}")
+                else:
+                    self.output.append(f"Message from {message.get_source()}: {text}")
             elif message.get_command() == "add_route":
                 address = message.get_param("address")
                 if address not in self.router.available_addresses():
@@ -86,6 +108,17 @@ class NetworkInterfaceController:
                 addr_to = message.get_source()
                 routing_table = message.get_param("routing_table")
                 self.router.add_routes(addr_to, routing_table.keys())
+
+            elif message.get_command() == "public_key":
+                cypher_text, shared_key = Kyber768.enc(bytes(message.get_param("public_key")))
+                print("Shared key is generated")
+                self.shared_keys[message.get_source()] = shared_key
+                self.send_cypher_text(message.get_source(),list(cypher_text))
+
+            elif message.get_command() == "cypher_text":
+                shared_key = Kyber768.dec(bytes(message.get_param("cypher_text")), self.private_keys[message.get_source()])
+                self.shared_keys[message.get_source()] = shared_key
+                print("Shared key is decoded from cypher_text")
 
             else:
                 self.output.append(f"Can't handle command: {message.get_command()}")
--- dabudimesh/shell.py
+++ dabudimesh/shell.py
@@ -2,19 +2,21 @@ from network import NetworkInterfaceController
 import bluetooth
 from cmd import Cmd
 from threading import Thread
+from cyphers.aescypher import AESCipher
 
 
 class DabudiShell(Cmd):
     type_help = "Type help or ? to list commands."
     intro = "Welcome to the DabudiMesh shell. " + type_help + "\n"
     prompt = "(dabudi) "
-
+    
     def __init__(self, nic: NetworkInterfaceController, messages: list = None):
         super().__init__()
         self.nic = nic
         self.running = True
         thread = Thread(target=self.__shed_messages, args=(messages,))
         thread.start()
+        self.connection_address = None
 
     def do_exit(self, arg):
         "exit : Terminate this program"
@@ -28,17 +30,30 @@ class DabudiShell(Cmd):
         args = self.__process(arg, 1)
         if args is not None:
             addr = args[0]
+            self.connection_address = addr
             connected = self.nic.connect_with(addr)
             if not connected:
                 print(f"Refused connection to {addr}")
-
     def do_message(self, arg):
         "message [addr] [msg] : Send message (msg) to node with address (addr)"
         args = self.__process(arg, 2)
         if args is not None:
             destination = args[0]
             text = args[1]
+            if destination in self.nic.shared_keys:
+                cypher = AESCipher(self.nic.shared_keys[destination])
+                message_encrypted = cypher.encrypt(text).decode("utf-8")
+                print("Message encrypted")
+                text = message_encrypted
             self.nic.send_text(destination, text)
+
+    def do_secure(self, arg):
+        "Security channel creating"
+        public_key = self.nic.public_key 
+        args = self.__process(arg, 1)
+        if args is not None:
+            destination = args[0]
+            self.nic.send_public_key(destination, public_key)
 
     def do_scan(self, arg):
         "scan : Discover nearby bluetooth devices"
